cd9245b565a4997bceb060137d801165438e2dc9
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-07-07 17:49:33 -0300

    development mode check

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 const {app, BrowserWindow} = require('electron')
 
+const isDev = process.env.NODE_ENV === "development" ? true : false ;
+
 function createWindow(){
     const win = new BrowserWindow({
         width: 777,
         height: 777,
         backgroundColor: '#12e321',
-        
     });
 
     win.loadFile('./src/index.html');
-    win.webContents.openDevTools();
-   
+
+    isDev ?  win.webContents.openDevTools() : console.log('not dev')
 }
 
 app.whenReady().then(()=>{
     console.log('app ready!');
-
     createWindow();
 })
